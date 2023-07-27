2dfdc92a91570319da6d89980c1fb2c09ac0e6e0
frontender43 <frontender43@gmail.com>
2023-07-27 20:34:31 +0200

    Здравствуйте! Изменил вычитание согласно пункта 6.

--- src/Stringcalculator.java
+++ src/Stringcalculator.java
@@ -7,7 +7,7 @@ public class Stringcalculator {
     static String result;
     static Scanner sc;
     static String input;
-static String[] str1;
+    static String[] str1;
     static String[] str;
 
     public static void main(String[] args) throws Exception {
@@ -15,24 +15,24 @@ static String[] str1;
 
 
         if (input.contains("+")) {
-            addition();}
+            addition();
+        } else if (input.contains("-")) {
+            subtraction();
+        } else if (input.contains("*")) {
+            multiplication();
+        } else if (input.contains("/")) {
+            division();
+        } else {
+            throw new Exception("Incorrect expression!!!");
+        }
 
-        else if (input.contains("-")) {
-            subtraction();}
+       if (result.toCharArray().length > 40) {
+           String res = lengthString40(result);
+           System.out.println("\"" + res + "..." + "\"");}
+            else
+            System.out.println("\"" + result + "\"");
+        }
 
-        else if (input.contains("*")) {
-            multiplication();}
-
-        else if (input.contains("/")) {
-            division();}
-
-        else {throw new Exception("Incorrect expression!!!");}
-
-        if (result.toCharArray().length > 40) {
-            System.out.println("\"" + result + "..." + "\"");}
-
-        else {System.out.println("\"" + result + "\"");}
-    }
 
 
     public static void initialization() throws Exception {
@@ -45,35 +45,43 @@ static String[] str1;
 
     public static void lengthString() throws Exception {
 
-            if (str[0].toCharArray().length > 10 || str[1].toCharArray().length > 10)  {
-                throw new Exception("Enter from 1 to 10 symbols");}
+        if (str[0].toCharArray().length > 10 || str[1].toCharArray().length > 10) {
+            throw new Exception("Enter from 1 to 10 symbols");
+        }
 
     }
 
 
-    public static void addition() throws Exception{
+    public static void addition() throws Exception {
         str = input.split("\\+");
         lengthString();
-        result = str[0] + str[str.length - 1];}
+        result = str[0] + str[str.length - 1];
+    }
 
 
-    public static void subtraction() throws Exception{
+    public static void subtraction() throws Exception {
         str = input.split("-");
         lengthString();
         int index = str[0].indexOf(str[1]);
-        if (index == -1) {result = str[0];}
-        else {result = str[0].substring(0,index).trim();}
+        if (index == -1) {
+            result = str[0].replace(str[0], "");
+        } else {
+            result = str[0].substring(0, index).trim();
         }
-
+    }
 
 
     public static void multiplication() throws Exception {
         str = input.split("\\*");
         lengthString();
         int number = Integer.parseInt(str[str.length - 1]);
-        if (number > 10) {throw new Exception("Enter numbers from 1 to 10 inclusive");}
+        if (number > 10) {
+            throw new Exception("Enter numbers from 1 to 10 inclusive");
+        }
 
-        for (int i = 0; i < number; i++) {result += str[0];}
+        for (int i = 0; i < number; i++) {
+            result += str[0];
+        }
     }
 
     public static void division() throws Exception {
@@ -82,15 +90,30 @@ static String[] str1;
         int length = str[0].toCharArray().length;
         int num = Integer.parseInt(str[str.length - 1]);
         if (num > 10) {
-            throw new Exception("Enter numbers from 1 to 10 inclusive");}
+            throw new Exception("Enter numbers from 1 to 10 inclusive");
+        }
         int res = length / num;
         if (res > 10) {
-            throw new Exception("Enter numbers from 1 to 10 inclusive");}
-        result = str[0].substring(0, length / num);}
+            throw new Exception("Enter numbers from 1 to 10 inclusive");
+        }
+        result = str[0].substring(0, length / num);
+    }
+
+    public static String lengthString40(String res) {
+        if (res.toCharArray().length > 40) {
+            char[] rrr = res.toCharArray();
+            char[] array = new char[40];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = rrr[i];
+
+            }
+
+             res = String.valueOf(array);
+        }
+        return res;
+    }
+
 }
-
-
-
 
 
 
